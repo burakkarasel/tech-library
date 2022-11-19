c8e23db9adde70fa553eb7b4c4948dea322b790a
kullop <burakcankarasel@gmail.com>
2022-11-20 00:01:25 +0300

    Created Spring Boot Back End application for the project, Created first entity

---
+++ back-end/src/main/java/com/kullop/techlibrary/TechLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.kullop.techlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLibraryApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/kullop/techlibrary/TechLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kullop.techlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
